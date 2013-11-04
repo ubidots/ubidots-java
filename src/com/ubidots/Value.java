53d633509cd75d78d215ff6dc243ef7ba8c0030f
Daniel da Silva <daniel@meltingwax.net>
2013-11-04 17:08:03 -0500

    Adding testGetValuesListSizeThree and fixing Value class so test passes

--- src/com/ubidots/Value.java
+++ src/com/ubidots/Value.java
@@ -9,15 +9,28 @@ public class Value extends ApiObject {
 	}
 	
 	public long getTimestamp() {
-		return getAttributeLong("timestamp").longValue();
+		Object o = getAttribute("timestamp");
+		
+		if (o instanceof Double) {
+			return ((Double)o).longValue();
+		} else if (o instanceof Integer) {
+			return ((Integer)o).longValue();
+		} else if (o instanceof Long) {
+			return ((Long)o).longValue();
+		} else {
+			throw new RuntimeException("Unexpected timestamp type: " + o.toString());
+		}
 	}
 
 	public double getValue() {
 		Object o = getAttribute("value");
+		
 		if (o instanceof Double) {
 			return ((Double)o).doubleValue();
 		} else if (o instanceof Integer) {
 			return ((Integer)o).doubleValue();
+		} else if (o instanceof Long) {
+			return ((Long)o).longValue();
 		} else {
 			throw new RuntimeException("Unexpected value type: " + o.toString());
 		}
--- src/com/ubidots/VariableTest.java
+++ src/com/ubidots/VariableTest.java
@@ -107,4 +107,44 @@ public class VariableTest {
 		
 		assertEquals(0, values.length);
 	}
+	
+	@Test
+	public void testGetValuesListSizeThree() {
+		ServerBridge bridge = mock(ServerBridge.class);
+		when(bridge.get("variables/a/values")).thenReturn("["
+				+ "{'id': 'xa', 'value': 5, 'timestamp': 1383602207000},"
+				+ "{'id': 'yb', 'value': 2, 'timestamp': 1383602208000},"
+				+ "{'id': 'zc', 'value': 7, 'timestamp': 1383602209000}"
+				+ "]");
+
+		ApiClient api = new ApiClient("abc");
+		api.setServerBridge(bridge);
+		
+		Map<String, Object> raw  = new HashMap<String, Object>();
+		raw.put("id", "a");
+		raw.put("name", "My Var");
+		raw.put("unit", "hPa");
+		raw.put("icon", "foobar");
+		
+		Variable variable = new Variable(raw, api);
+		Value[] values = variable.getValues();
+		
+		assertEquals(3, values.length);
+		verify(bridge).get("variables/a/values");
+		
+		// Check ids
+		assertEquals("xa", values[0].getId());
+		assertEquals("yb", values[1].getId());
+		assertEquals("zc", values[2].getId());
+		
+		// Check values
+		assertTrue(Math.abs(values[0].getValue() - 5.0) < .1);
+		assertTrue(Math.abs(values[1].getValue() - 2.0) < .1);
+		assertTrue(Math.abs(values[2].getValue() - 7.0) < .1);
+
+		// Check timestamps
+		assertEquals(1383602207000l, values[0].getTimestamp());
+		assertEquals(1383602208000l, values[1].getTimestamp());
+		assertEquals(1383602209000l, values[2].getTimestamp());
+	}
 }

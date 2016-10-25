76afe6dac0ad87d7517cc9301e83a0a1d71f6676
David Sánchez <juanda95@gmail.com>
2016-10-25 18:20:16 -0500

    Adding methods to change baseUrl
    There was no way to change the default URL when using a token instead
    of the API Key. These methods were added to overcome that.

--- src/main/java/com/ubidots/ApiClient.java
+++ src/main/java/com/ubidots/ApiClient.java
@@ -9,9 +9,9 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class ApiClient {
-	
+
 	private ServerBridge bridge;
-	
+
 	public ApiClient(String apiKey) {
 		bridge = new ServerBridge(apiKey);
 	}
@@ -20,7 +20,7 @@ public class ApiClient {
 		bridge = new ServerBridge(apiKey, baseUrl);
 	}
 
-    public ApiClient() {}
+	public ApiClient() {}
 
 	ServerBridge getServerBridge() {
 		return bridge;
@@ -31,77 +31,81 @@ public class ApiClient {
 		return this;
 	}
 
+	public void setBaseUrl(String baseUrl) {
+		bridge.setBaseUrl(baseUrl);
+	}
+
 	public DataSource[] getDataSources() {
 		String json = bridge.get("datasources");
-		
+
 		Gson gson = new Gson();
 		List<Map<String, Object>> rawDataSources = gson.fromJson(json, List.class);
-		
+
 		DataSource[] dataSources = new DataSource[rawDataSources.size()];
-		
+
 		for (int i = 0; i < rawDataSources.size(); i++) {
 			dataSources[i] = new DataSource(rawDataSources.get(i), this);
 		}
 
 		return dataSources;
 	}
-	
+
 	public DataSource getDataSource(String id) {
 		String json = bridge.get("datasources/" + id);
-		
+
 		Gson gson = new Gson();
 		Map<String, Object> rawDataSource = (Map<String, Object>) gson.fromJson(json, Map.class);
-		
+
 		if (rawDataSource.containsKey("detail"))
 			return null;
 		else
 			return new DataSource(rawDataSource, this);
 	}
-	
+
 	public DataSource createDataSource(String name) {
 		return createDataSource(name, null, null);
 	}
-	
+
 	public DataSource createDataSource(String name, Map<String, String> context, List<String> tags) {
 		Map<String, Object> data = new HashMap<String, Object>();
 		data.put("name", name);
-		
-		if (context != null) 
+
+		if (context != null)
 			data.put("context", context);
-		
+
 		if (tags != null)
 			data.put("tags", tags);
-		
+
 		Gson gson = new Gson();
 		String json = bridge.post("datasources/", gson.toJson(data));
 		Map<String, Object> rawDataSource = (Map<String, Object>) gson.fromJson(json, Map.class);
-		
+
 		DataSource ds = new DataSource(rawDataSource, this);
 
 		return ds;
 	}
-	
+
 	public Variable[] getVariables() {
 		String json = bridge.get("variables");
-		
+
 		Gson gson = new Gson();
 		List<Map<String, Object>> rawVariables = gson.fromJson(json, List.class);
-		
+
 		Variable[] variables = new Variable[rawVariables.size()];
-		
+
 		for (int i = 0; i < rawVariables.size(); i++) {
 			variables[i] = new Variable(rawVariables.get(i), this);
 		}
-		
+
 		return variables;
 	}
 
 	public Variable getVariable(String id) {
 		String json = bridge.get("variables/" + id);
-		
+
 		Gson gson = new Gson();
 		Map<String, Object> rawVariable = (Map<String, Object>) gson.fromJson(json, Map.class);
-		
+
 		if (rawVariable.containsKey("detail"))
 			return null;
 		else
